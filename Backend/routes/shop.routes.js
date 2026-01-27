ba3ccce65b185dd0e4fa849305915b1f5e76bb4d
ashutosh_mishra2812 <2k23.csaiml2313890@gmail.com>
2026-01-27 08:47:45 +0530

    owner backend thing arre done

---
+++ Backend/routes/shop.routes.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import isAuth from '../middlewares/isAuth.js';
+import { createEditShop } from '../controllers/shop.controllers.js';
+import { upload } from '../middlewares/multer.js';
+
+
+const shopRouter = express.Router();
+
+
+shopRouter.get("/create-edit", isAuth,upload.single("image"),createEditShop);
+
+
+
+export default shopRouter;
